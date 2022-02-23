2de0ae6b115c325e0e16c206fb8d1232f783bd13
akashwebs <aksah.shil227@gmail.com>
2022-02-23 22:45:52 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const serachButton = () => {
+    const input = document.getElementById('input-value')
+    const inputValue = parseInt(input.value);
+    if (isNaN(inputValue)) {
+        console.log(inputValue);
+    }
+    console.log(inputValue);
+
+}
