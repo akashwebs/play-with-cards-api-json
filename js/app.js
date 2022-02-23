2c8d89d392df735210abe5f9d785f6ab11004701
akashwebs <aksah.shil227@gmail.com>
2022-02-23 22:56:18 +0600

    added error handaling

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 const serachButton = () => {
     const input = document.getElementById('input-value')
+    const errorMsg = document.getElementById('error')
     const inputValue = parseInt(input.value);
-    if (isNaN(inputValue)) {
-        console.log(inputValue);
+    if (isNaN(inputValue) || inputValue == '') {
+        errorMsg.innerText = 'Please give a number'
+        errorMsg.className = 'text-danger' + ' text-center'
+    } else if (inputValue < 0) {
+        errorMsg.innerText = 'plesae give a positive number';
     }
     console.log(inputValue);
+    input.value = ''
 
 }
